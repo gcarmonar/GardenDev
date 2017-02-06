7050943432720b1251abf9291f802a5b8746fb7d
Gerardo Carmona <gcarmonar@gmail.com>
2017-02-06 14:24:44 -0600

    Init
    This repo is to helpme though the development of the first prototype. I
    am using Github because I am using different computers to work on the
    code. Final release will be in another separated repository.

---
+++ Arduino/GardenMain/GardenMain.ino
@@ -0,0 +1,140 @@
+#define MOIST_PIN         A0
+#define TEMP_PIN          A1
+#define LIGHT_PIN         A2
+#define HIGH_TEMP_LED_PIN 6
+#define LOW_TEMP_LED_PIN  5
+
+#define NUMBER_SAMPLES    60/5 //One minute / 5 seconds each sample
+#define TASK_50_MS        50
+#define TASK_500_MS       500
+#define TASK_5_SEC        1000*5  //Sample time
+#define TASK_1_MIN        1000*60 //Send data to display and Raspberry Pi
+#define HIGH_TEMP         225
+#define LOW_TEMP          210
+
+unsigned long task_50_ms =  0;
+unsigned long task_500_ms = 0;
+
+int task50msFlag =          0;
+int task500msFlag =         0;
+int sample =                0;
+
+int tempArray[NUMBER_SAMPLES] =   {};
+int moistArray[NUMBER_SAMPLES] =  {};
+int lightArray[NUMBER_SAMPLES] =  {};
+
+
+
+
+void Task50ms();
+void Task500ms();
+void ReadTemp();
+void ReadMoist();
+void ReadLight();
+int GetAverage(int data[]);
+
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(HIGH_TEMP_LED_PIN, OUTPUT);
+  pinMode(LOW_TEMP_LED_PIN, OUTPUT);
+}
+
+void loop(){
+  
+  if (TASK_50_MS <= (millis()-task_50_ms)){
+    task50msFlag = 1;
+    task_50_ms = millis();
+  }
+
+  if (TASK_500_MS <= (millis()- task_500_ms)){
+    task500msFlag = 1;
+    task_500_ms = millis();
+  }
+
+  if (task50msFlag){
+    Task50ms();
+    task50msFlag = 0;
+  }
+
+  if (task500msFlag){
+    Task500ms();
+    task500msFlag = 0;
+  }
+
+}
+
+
+void Task50ms(){
+  if (sample == NUMBER_SAMPLES){
+    sample = 0;
+  }
+  ReadMoist();
+  ReadTemp();
+  ReadLight();
+  sample++;
+}
+
+
+void Task500ms(){
+  int temp = GetAverage(tempArray);
+  int moist = GetAverage(moistArray);
+  int light = GetAverage(lightArray);
+
+  Serial.print("Temp [C]: ");
+  Serial.println(temp);
+  Serial.print("Moisture: ");
+  Serial.println(moist);
+  Serial.print("Light: ");
+  Serial.println(light);
+  Serial.println("-----------------");
+
+  StatusLeds(temp, moist, light);
+}
+
+
+void ReadTemp(){
+  int input = analogRead(TEMP_PIN);
+  float temp = (((float)input*0.488) - 50.0)*10;
+
+  tempArray[sample] = (int)temp;
+}
+
+
+void ReadLight(){
+  int input = analogRead(LIGHT_PIN);
+  lightArray[sample] = input;
+}
+
+void ReadMoist(){
+  int input = analogRead(MOIST_PIN);
+  input = 0;
+  moistArray[sample] = input;
+}
+
+
+int GetAverage(int data[]){
+  int avg=0;
+
+  for (int i = 0; i < NUMBER_SAMPLES; i++){
+    avg += data[i];
+  }
+
+  avg = avg / NUMBER_SAMPLES;
+
+  return avg;
+}
+
+void StatusLeds(int t, int m, int l){
+  if (t <= LOW_TEMP){
+    digitalWrite(LOW_TEMP_LED_PIN, HIGH);
+    digitalWrite(HIGH_TEMP_LED_PIN, LOW);
+  }else if (t >= HIGH_TEMP){
+    digitalWrite(LOW_TEMP_LED_PIN, LOW);
+    digitalWrite(HIGH_TEMP_LED_PIN, HIGH);
+  }else{
+    digitalWrite(LOW_TEMP_LED_PIN, LOW);
+    digitalWrite(HIGH_TEMP_LED_PIN, LOW);
+  }
+
+}
---
+++ RaspberryPi/GardenSaveLogs.py
@@ -0,0 +1,69 @@
+
+
+'''
+Receives serial data from Arduino, saves the data in a local database and 
+sends a Twitter every hour aprox with the actual status of the garden. 
+
+
+http://gardenbot.org/howTo/soilMoisture/
+https://learn.sparkfun.com/tutorials/raspberry-pi-twitter-monitor?_ga=1.248133262.863112450.1472145466
+https://github.com/ryanmcgrath/twython/blob/master/examples/update_status.py
+http://pyserial.readthedocs.io/en/latest/shortintro.html
+https://docs.python.org/2/library/time.html
+https://www.tutorialspoint.com/python/python_date_time.htm
+https://www.tutorialspoint.com/python/python_files_io.htm
+
+'''
+
+#--- Libraries ------------------------------------------------------------
+#import serial
+#from twython import Twython, TwythonError
+import time
+
+#--- Objects --------------------------------------------------------------
+#ser = serial.Serial('/dev/ttyUSB0')
+
+
+
+
+# def GetLocalTime():
+# 	localtime = time.localtime(time.time())
+# 	year = localtime[0]
+# 	month = localtime[1]
+# 	day = localtime[2]
+# 	hour = localtime[3]
+# 	minutes = localtime[4]
+# 	seconds = localtime[5]
+# 	return localtime
+
+
+
+#data = ser.readline()
+data = "Dod"
+
+if data[0] == "D":
+	localtime = time.localtime(time.time())
+	year = localtime[0]
+	month = localtime[1]
+	day = localtime[2]
+	hour = localtime[3]
+	minutes = localtime[4]
+	seconds = localtime[5]
+	timeText = str(year) + '/' + str(month) + '/' + str(day) + ' ' + str(hour) + ':' + str(minutes) + ':' + str(seconds)  
+	#sensors = ser.readline()
+	sensors = "t218 m915 l887"
+	text = sensors.split()
+	temp = (int(text[0][1:]))/10.0
+	text = timeText + "," + str(temp) + "," + (text[1][1:]) + "," + (text[2][1:])
+	print text 
+
+
+
+	# lt = time.localtime(time.time())
+	# file_name = "Garden_" + str(lt[0]) + "_" + str(lt[1]) + "_" + str(lt[2]) + "___" + str(lt[3])
+	# fo = open(file_name, "a")
+	# fo.write()
+
+
+
+
