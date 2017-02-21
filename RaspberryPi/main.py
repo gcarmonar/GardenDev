ee6adc62d261b7db3ca3168cd36cd43b5d2174db
Gerardo Carmona <gcarmonar@gmail.com>
2017-02-21 16:31:13 -0600

    Main Python File
    First release of the main file. Not tested on target.

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -2,63 +2,154 @@
 
 
 #=== Libraries =============================================================
-
-#=== Objects ===============================================================
+import time
+import serial
 
 #=== Constants =============================================================
+SERIAL_DATA_AVAILABLE = 1
+PROCESSED_VALID_DATA = 1
+LOG_SUCCESS	= 1
+# Serial
+SERIAL_PORT	= 'COM7'
+SERIAL_BAUD = 9600
+SERIAL_TIMEOUT = 1
+
+#=== Objects ===============================================================
+ser = serial.Serial(SERIAL_PORT, SERIAL_BAUD, timeout=SERIAL_TIMEOUT)
 
 #=== Variables =============================================================
+# Dictionaries
+actualDate = {}
+lastDate = {}
+# Flags
+serialFlag = 0
+dataFlag = 0
+logFlag = 0
+# Global general variables
+dateString = ''
+logName = ''
 
 #=== Functions =============================================================
 def SaveLog():
 	print "Save Log"
 
 
+
 def SendTwitter():
 	print "Send Twitter"
 
 def ReadSerialPort():
-	print "Read Serial Port"
+	# print "Read Serial Port"
+	data = ser.read(100)
+	
+	if len(data) > 0:
+		# print data
+		if data[0] == "D":
+			# print "Last is: " + data[-3:-2] + "\n"
+			if data[-3:-2] == "A":	# Last char before eol
+				#serialFlag = 1
+				return 1
+	return 0
+	#ser.flush()
 
-def UpdateTime():
+def UpdateDate():
 	print "Get Time"
 	localtime = time.localtime(time.time())
-	year = localtime[0]
-	month = localtime[1]
-	day = localtime[2]
-	hour = localtime[3]
-	minutes = localtime[4]
-	seconds = localtime[5]
-
-	# Correct format
-		if hour < 10:
-			hour = '0'+str(hour)
-		if minutes < 10:
-			minutes = '0'+str(minutes)
-		if seconds < 10:
-			seconds = '0'+str(seconds)
 	
+	actualDate['year'] = localtime[0]
+	actualDate['month'] = localtime[1]
+	actualDate['day'] = localtime[2]
+	actualDate['hour'] = localtime[3]
+	actualDate['minutes'] = localtime[4]
+	actualDate['seconds'] = localtime[5]
 
 def ProcessData():
-	print "Process Data"
+	print "Processing Data"
+
+	# year = localtime[0]
+	# month = localtime[1]
+	# day = localtime[2]
+	# hour = localtime[3]
+	# minutes = localtime[4]
+	# seconds = localtime[5]
+
+	# # Correct format
+	# 	if hour < 10:
+	# 		hour = '0'+str(hour)
+	# 	if minutes < 10:
+	# 		minutes = '0'+str(minutes)
+	# 	if seconds < 10:
+	# 		seconds = '0'+str(seconds)
+
+	dateString = str(actualTime['year']) + '_'
+	dateString += str(actualTime['month']) + '_'
+	dateString += str(actualTime['day']) + '_'
+	dateString += '___'
+
+	if actualTime['hour'] < 10:
+		dateString += '0' + str(actualTime['hour']) + '_'
+	else:
+		dateString += str(actualTime['hour']) + '_'
+
+	if actualTime['minutes'] < 10:
+		dateString += '0' + str(actualTime['minutes']) + '_'
+	else:
+		dateString += str(actualTime['minutes']) + '_'
+
+	logName = dateString
+
+	if actualTime['seconds'] < 10:
+		dateString += '0' + str(actualTime['seconds']) + '_'
+	else:
+		dateString += str(actualTime['seconds']) + '_'
+
+
+
+	return 1
 
 
 
 #=== Main ==================================================================
-def main:
+def main():
+	# Init
 	print "Main"
+	global serialFlag
+
+	UpdateDate()
+	lastDate = actualDate
+
 	while True:
-		lastTime = Time
-		time = UpdateTime()
+		# Always do it
+		timer = time.time()
+		ReadSerialPort()
+		UpdateDate()
+
+		# If hour has changed then send a Twitt
+		if lastDate['hour'] != actualDate['hour']:
+			#SendTwitter()
+			lastDate = actualDate
+
+		if serialFlag == SERIAL_DATA_AVAILABLE:
+			serialFlag = 0
+			dataFlag  = ProcessData()
+			if dataFlag == PROCESSED_VALID_DATA:
+				logFlag = SaveLog()
+				if logFlag != LOG_SUCCESS:
+					print "Error saving data"
+
+
 		
-		if lastTime.Hour != time.hour:
-			SendTwitter()
+		
+		
+
 
 
 try:  
 	main() 
-except KeyboardInterrupt:  
-	GPIO.cleanup()       # clean up GPIO on CTRL+C exit
-except Exception: 
-	pass
-GPIO.cleanup()           # clean up GPIO on normal exit 
+except KeyboardInterrupt:
+	ser.close()  
+	#GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+	print "KeyboardInterrupt"
+# except Exception: 
+# 	pass
+#GPIO.cleanup()           # clean up GPIO on normal exit 
