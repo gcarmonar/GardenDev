379e866fd6d383f2b5a0555b1f811626cc80e36f
Gerardo Carmona <gcarmonar@gmail.com>
2017-02-23 16:55:45 -0600

    Update to main.py

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -25,9 +25,22 @@ LOG_SUCCESS				= 1
 LOG_ERROR				= 0
 # Serial
 SERIAL_PORT				= 'COM7'			# Windows
-SERIAL_PORT				= '/dev/ttyAMA0'	# Raspberry Pi
+#SERIAL_PORT				= '/dev/ttyAMA0'	# Raspberry Pi
 SERIAL_BAUD 			= 9600
 SERIAL_TIMEOUT 			= 1					# In seconds
+# Alerts
+TEMPERATURE 			= 0
+MOISTURE 				= 1
+MAX_TEMP				= 24.3				# Configurable
+MIN_TEMP				= 22.2				# Configurable
+HIGH_TEMP_ALERT			= 2
+LOW_TEMP_ALERT			= 1
+NO_TEMP_ALERT			= 0
+MIN_SOIL_MOISTURE		= 500				# Configurable, try and error
+LOW_SOIL_MOISTURE_ALERT = 1
+NO_SOIL_MOISTURE_ALERT	= 0
+
+
 
 #=== Objects ===============================================================
 ser = serial.Serial(SERIAL_PORT, SERIAL_BAUD, timeout=SERIAL_TIMEOUT)
@@ -42,41 +55,77 @@ lastDate = {}
 serialFlag = 0
 dataFlag = 0
 logFlag = 0
+twitterAlertFlag = 0
 # Global general variables
 dateString = ''
-logName = ''
+logFileName = ''
+pictureName = ''
+alerts = [NO_TEMP_ALERT,NO_SOIL_MOISTURE_ALERT]
+serialData = []
 
 #=== Functions =============================================================
 def SaveLog():
 	print "Save Log"
 
+	global logFileName, dateString, serialData
+	
+	fp = open(logFileName, "a")
+	fp.write(dateString)
+	for word in serialData:
+		fp.write(',')
+		fp.write(str(word))
+	fp.write('\n')
+	fp.close()
+
+	return LOG_SUCCESS
+
 def TakePicture():
 	print "Take Picture"
-	pictureName = dateString + '.jpg'
-	camera.capture(pictureName)
+	# pictureName = dateString + '.jpg'
+	# camera.capture(pictureName)
 
 def SendTwitter():
 	print "Send Twitter"
 	# photo = open('images/image.jpg', 'rb')	
 	# response = twitter.upload_media(media=photo)
-	# twitter.update_status(status='Checkout this cool image!', media_ids=[response['media_id']])
+	# # Add Temperature, soil moisture and light to message
+	# message = "Hi, the temperature is %s C"
+	# twitter.update_status(status=message, media_ids=[response['media_id']])
+
+
+
+	if alerts[TEMPERATURE] != NO_TEMP_ALERT:
+		# Send alert temp is not good
+		twitterAlertFlag |= 1
+	if alerts[MOISTURE] != LOW_SOIL_MOISTURE_ALERT:
+		twitterAlertFlag |= 2 
+
+	if (twitterAlertFlag & 4) == 1:
+		twitterAlertFlag = 0
 
 def ReadSerialPort():
-	# print "Read Serial Port"
+	print "Read Serial Port"
+
+	global serialData
+
 	data = ser.read(100)
 	
 	if len(data) > 0:
 		# print data
 		if data[0] == "D":
 			# print "Last is: " + data[-3:-2] + "\n"
-			if data[-3:-2] == "E":	# Last char before eol
-				#serialFlag = 1
+			if data[-3:-2] == "E":				# Last char before eol
+				# Save data into global var and split into a list
+				serialData = data[2:-4].split(',')	
 				return SERIAL_DATA_AVAILABLE
 		return SERIAL_DATA_ERROR
 	return NO_SERIAL_DATA
 
 def UpdateDate():
 	print "Get Time"
+
+	global actualDate
+
 	localtime = time.localtime(time.time())
 	
 	actualDate['year'] = localtime[0]
@@ -89,27 +138,59 @@ def UpdateDate():
 def ProcessData():
 	print "Processing Data"
 
-	dateString = str(actualTime['year']) + '_'
-	dateString += str(actualTime['month']) + '_'
-	dateString += str(actualTime['day']) + '_'
+	global dateString, logFileName, pictureName, serialData, alerts
+
+	# Process time for saving log file
+	dateString = str(actualDate['year']) + '_'
+	dateString += str(actualDate['month']) + '_'
+	dateString += str(actualDate['day']) + '_'
 	dateString += '___'
 
-	if actualTime['hour'] < 10:
-		dateString += '0' + str(actualTime['hour']) + '_'
+	if actualDate['hour'] < 10:
+		dateString += '0' + str(actualDate['hour']) + '_'
 	else:
-		dateString += str(actualTime['hour']) + '_'
+		dateString += str(actualDate['hour']) + '_'
 
-	if actualTime['minutes'] < 10:
-		dateString += '0' + str(actualTime['minutes']) + '_'
+	logFileName = dateString[:-1] + '.log'
+
+	if actualDate['minutes'] < 10:
+		dateString += '0' + str(actualDate['minutes']) + '_'
 	else:
-		dateString += str(actualTime['minutes']) + '_'
+		dateString += str(actualDate['minutes']) + '_'
 
-	logName = dateString
+	pictureName = dateString[:-1] + '.jpg'
 
-	if actualTime['seconds'] < 10:
-		dateString += '0' + str(actualTime['seconds']) + '_'
+	if actualDate['seconds'] < 10:
+		dateString += '0' + str(actualDate['seconds'])
 	else:
-		dateString += str(actualTime['seconds']) + '_'
+		dateString += str(actualDate['seconds'])
+
+	# Process Serial Data
+	# Check for extreme temperatures
+	# Check for Dry soil
+	print "Serial data: "
+	print serialData
+	for i in range(0,len(serialData),2):
+		#before comparing, preferable to convert to floats and scale data
+
+		if serialData[i][0] == 't':
+			serialData[i+1] = int(serialData[i+1]) / 10.0
+		elif serialData[i][0] == 'm' or serialData[i][0] == 'r':
+			serialData[i+1] = int(serialData[i+1])
+
+		if serialData[i][0] == 't':
+			if serialData[i+1] > MAX_TEMP:
+				alerts[TEMPERATURE] = HIGH_TEMP_ALERT
+			elif serialData[i+1] < MIN_TEMP:
+				alerts[TEMPERATURE] = LOW_TEMP_ALERT
+			else:
+				alerts[TEMPERATURE] = NO_TEMP_ALERT
+		
+		elif serialData[i][0] == 'm':
+			if serialData[i+1] < MIN_SOIL_MOISTURE:
+				alerts[MOISTURE] = LOW_SOIL_MOISTURE_ALERT
+			else:
+				alerts[MOISTURE] = NO_SOIL_MOISTURE_ALERT
 
 	return 1
 
@@ -143,14 +224,10 @@ def main():
 				if logFlag != LOG_SUCCESS:
 					print "Error saving data"
 
-
-		
-		
-		
+		time.sleep(2)
 
 
-
-try:  
+try:
 	main() 
 except KeyboardInterrupt:
 	ser.close()  
